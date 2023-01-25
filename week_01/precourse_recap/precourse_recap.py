38d1dc9e86d1aa4594b5594a11a07af8308295b9
LaurenLingham <lauren.lingham@outlook.com>
2023-01-25 21:00:28 +0000

    initial homework

---
+++ week_01/precourse_recap/precourse_recap.py
@@ -0,0 +1,17 @@
+a = 5
+b = True
+c = "Tuesday"
+
+def hello():
+    print("Hello world!  Today is " + c + ".")
+
+hello()
+
+def easy_sum():
+    return a * 10 - 3
+
+def easy_sum_two():
+    total = a * 2
+    print(total)
+
+easy_sum_two()
